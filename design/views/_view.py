2e8a1b85b8170d704a41172edd54f99b07ad6f33
KayJayx <73000710+KayJayx@users.noreply.github.com>
2024-07-06 15:20:54 -0700

    Update _view.py

--- design/views/_view.py
+++ design/views/_view.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 from design.views._designer import Designer
 import design.views.button_view as bv
 import design.views.slider_view as sv
+import design.views.label_view as lv
+import design.views.checkbox_view as cv
 import typing
 
 class View():
@@ -16,11 +18,16 @@ class View():
         super().__init__()
         self.designer               = Designer()
         self.gen_button_view        = bv.GenerateWaveformButtonView(self.designer.generate_waveform_button)
+        self.stop_button_view       = bv.StopWaveformButtonView(self.designer.stop_waveform_button)
+        self.clear_button_view      = bv.ClearWaveformButtonView(self.designer.clear_plot_button)
         self.resolution_slider_view = sv.ResolutionSliderView(self.designer.resolution_slider)
         self.amplitude_slider_view  = sv.AmplitudeSliderView(self.designer.amplitude_slider)
         self.height_slider_view     = sv.HeightSliderView(self.designer.height_slider)
         self.phase_slider_view      = sv.PhaseSliderView(self.designer.phase_slider)
         self.frequency_slider_view  = sv.FrequencySliderView(self.designer.frequency_slider)
+        self.angular_label_view     = lv.AngularLabelView(self.designer.angular_label)
+        self.period_label_view      = lv.PeriodLabelView(self.designer.period_label)
+        self.norm_checkbox_view     = cv.NormalizeCheckboxView(self.designer.normalize_freq)
 
     @property
     def get_frequency(self) -> float:
